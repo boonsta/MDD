1c56211f0fc8e4bf3f827289cdd84070bbbf29d0
boonsta <boonsta@fullsail.edu>
2013-05-23 10:27:46 -0700

    updated geolocate

--- fuel/app/classes/controller/geolocate.php
+++ fuel/app/classes/controller/geolocate.php
@@ -1,23 +1,84 @@
-<?php
 
-$this->load->library('googlemaps');
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Geolocation</title>
+    <!-- Set basic styling for the map div -->
+    <style>
+        #map {
+            width:400px;
+            height:300px;
+        }
+    </style>
+    <!-- Load the Google APIs -->
+    <script src="http://www.google.com/jsapi"></script>
+    <script>
+        // Load the map scripts
+        google.load('maps', '3', {other_params:'sensor=true'});
 
-$config = array();
-$config['center'] = 'auto';
-$config['onboundschanged'] = 'if (!centerGot) {
-       var mapCenter = map.getCenter();
-       marker_0.setOptions({
-           position: new
-google.maps.LatLng(mapCenter.lat(), mapCenter.lng())
-       });
-}
-centerGot = true;';
-$this->googlemaps->initialize($config);
-//set up the marker ready for positioning
-//once we know the users location
-$marker = array();
-$this->googlemaps->add_marker($marker);
+        // Function to create a map and check for geolocation
+        function mapInit() {
 
-$data['map'] = $this->googlemaps->create_map();
-$this->load->view('geolocate', $data)
-?>
+            // Set the options to be used when creating the map
+            var myOptions = {
+                    zoom: 0,
+                    center: new google.maps.LatLng(0, 0),
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+
+            // Create the new
+            map = new google.maps.Map(document.getElementById("map"), myOptions);
+
+            // Check if the browser supports geolocation
+            if(navigator.geolocation) {
+
+                navigator.geolocation.getCurrentPosition(currentPositionCallback);
+                
+            } else {
+
+                alert('The browser does not support geolocation');
+
+            }
+
+        }
+
+        function currentPositionCallback(position) {
+
+            // Create a new latlng based on the latitude and longitude from the user's position
+            var user_lat_long = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+            // Add a marker using the user_lat_long position
+            var marker = new google.maps.Marker({
+                position: user_lat_long,
+                map: map
+            });
+
+            // Set the center of the map to the user's position and zoom into a more detailed level
+            map.setCenter(user_lat_long);
+            map.setZoom(15);
+
+        }
+
+        google.setOnLoadCallback(mapInit);
+        
+    </script>
+</head>
+<body>
+
+    <h1>Geolocation</h1>
+    
+    <div id="map"></div>
+    
+    <script type="text/javascript">
+        var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+        document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+    </script>
+    <script type="text/javascript">
+        try {
+            var pageTracker = _gat._getTracker("UA-18038221-1");
+            pageTracker._trackPageview();
+        } catch(err) {}
+    </script>
+
+</body>
+</html>
--- fuel/app/views/geolocate/geolocate.php
+++
@@ -1,9 +0,0 @@
-<html>
-<head>
-    <script type="text/javascript">
-        var centerGot = false;
-    </script>
-    <?php echo $map['js']; ?>
-</head>
-<body><?php echo $map['html']; ?></body>
-</html>
