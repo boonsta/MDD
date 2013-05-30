e9d6acb29ee0e8c0c7a3ca85f745ae1b010a2b10
boonsta <boonsta@fullsail.edu>
2013-05-30 01:01:55 -0700

    sql dump and logout update

--- fuel/app/views/login/create.php
+++
@@ -1,9 +0,0 @@
-<h2>New <span class='muted'>Login</span></h2>
-<br>
-
-<?php echo render('login/_form'); ?>
-
-
-<p><?php echo Html::anchor('signin/create', 'Back'); ?>
-   <?php echo Html::anchor('signin/create', 'Sign Up'); ?>
-</p>
--- fuel/app/views/signin/edit.php
+++
@@ -1,8 +0,0 @@
-<h2>Editing <span class='muted'>Signin</span></h2>
-<br>
-
-<?php echo render('signin/_form'); ?>
-<p>
-	<?php echo Html::anchor('signin/view/'.$signin->id, 'View'); ?> |
-	<?php echo Html::anchor('signin', 'Back'); ?></p>
-	
